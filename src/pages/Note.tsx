4bad794039f06309c08530f7d1b7c6a08846b7fa
Daniel Oshiro <daniel.oshiro@protonmail.com>
2024-04-20 17:47:19 +0100

    feat: load note

--- src/pages/Note.tsx
+++ src/pages/Note.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router";
+import Context from "../state";
+import Textarea from "../components/Textarea";
 
 interface RouteParams {
   [key: string]: string | undefined;
@@ -6,10 +9,52 @@ interface RouteParams {
 }
 
 const Note = () => {
-  const { id } = useParams<RouteParams>();
+  const { id, session } = useParams<RouteParams>();
+  const { loadNote } = useContext(Context);
+  const [currentNote, setCurrentNote] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    getCurrentNote();
+  }, []);
+
+  const getCurrentNote = async () => {
+    if (!session?.length || !id?.length) return;
+
+    const resNote = await loadNote({
+      sessionId: session,
+      noteId: id,
+    });
+    setCurrentNote(resNote.body);
+    setLoading(false);
+  };
+
+  const saveNote = () => {
+    console.log("save");
+  };
+
+  const handleChange = (value: string) => {
+    setCurrentNote(value);
+  };
+
+  if (loading) return <p>Loading...</p>;
   return (
-    <div>
-      <p>Route id: {id}</p>
+    <div className="container h-full flex flex-col">
+      <form onSubmit={saveNote} className="flex flex-col h-full relative">
+        <Textarea
+          label="Add note"
+          id="note"
+          onChange={handleChange}
+          value={currentNote}
+          hideLabel
+          noBorder
+        />
+        <div>
+          <button className="bg-white border hover:bg-slate-300 text-black font-semibold py-2 px-4 rounded">
+            Add
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
