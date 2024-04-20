20a3f9da632dd9128b5b46737c908b7b95aa2a0b
Daniel Oshiro <daniel.oshiro@protonmail.com>
2024-04-20 12:10:18 +0100

    feat: simple textarea component

---
+++ src/components/Textarea.tsx
@@ -0,0 +1,39 @@
+import { ChangeEvent, useState, FC } from "react";
+
+interface InputProps {
+  placeholder?: string;
+  value?: string;
+  label: string;
+  id: string;
+  onChange?: (value: string) => void;
+}
+
+const Input: FC<InputProps> = ({ placeholder, value, onChange, label, id }) => {
+  const [inputValue, setInputValue] = useState(value || "");
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+    if (onChange) {
+      onChange(newValue);
+    }
+  };
+
+  return (
+    <div className="mb-4">
+      <label htmlFor={id} className="pb-2 block">
+        {label}
+      </label>
+      <textarea
+        className="w-full p-2 rounded border border-slate-300 focus:outline-none focus:border-blue-500"
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={handleChange}
+        name={id}
+        id={id}
+      />
+    </div>
+  );
+};
+
+export default Input;
