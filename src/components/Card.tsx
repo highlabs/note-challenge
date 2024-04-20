8211380788012bbc6363c1e7282c4f530a6e849e
Daniel Oshiro <daniel.oshiro@protonmail.com>
2024-04-20 12:40:48 +0100

    feat: add support to extra css classes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,11 +2,14 @@ import { FC, ReactNode } from "react";
 
 interface CardType {
   children: ReactNode;
+  className?: string;
 }
 
-export const Card: FC<CardType> = ({ children }) => {
+export const Card: FC<CardType> = ({ children, className }) => {
   return (
-    <div className="bg-white border rounded p-4 max-w-lg mx-auto">
+    <div
+      className={`bg-white border rounded p-4 max-w-lg mx-auto ${className}`}
+    >
       {children}
     </div>
   );
