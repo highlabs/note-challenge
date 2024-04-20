1811ea77816d7f7e28dce20dec0632d30d937360
Daniel Oshiro <daniel.oshiro@protonmail.com>
2024-04-20 11:00:21 +0100

    feat: simple slugify functions

---
+++ src/utils/slugify.tsx
@@ -0,0 +1,9 @@
+const slugify = (text: string): string => {
+  return text
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
+
+export default slugify;
