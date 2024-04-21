45b2827bf23a9bed939cc33e91e17569cbcdabb6
Daniel Oshiro <daniel.oshiro@protonmail.com>
2024-04-21 09:56:22 +0100

    feat: avoid dropdown to be outside window

--- src/components/Mentions.tsx
+++ src/components/Mentions.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, MouseEvent, useEffect, useRef, useState } from "react";
 import { User } from "../utils/types";
 
 interface MentionsType {
@@ -11,6 +11,13 @@ interface MentionsType {
 }
 
 const Mentions: FC<MentionsType> = ({ position, persons, mention }) => {
+  const [showOnTop, setShowOnTop] = useState<boolean>(false);
+  const [showOnRight, setShowOnRight] = useState<boolean>(false);
+  const [mentionHandles, setMentionHandles] = useState<User[]>([]);
+
+  useEffect(() => checkIfDropdownFits(), [mentionHandles]);
+  useEffect(() => handlePersons(), [mention]);
+
   const handlePersons = () => {
     const sortedPersons = persons.sort((a, b) => {
       if (a.first_name < b.first_name) {
@@ -28,22 +35,62 @@ const Mentions: FC<MentionsType> = ({ position, persons, mention }) => {
       const nameStartWith = sortedPersons.filter((name) =>
         name.first_name.startsWith(nameWithoutHandle)
       );
-      return nameStartWith.slice(0, 10);
+      setMentionHandles(nameStartWith.slice(0, 10));
     }
 
-    return sortedPersons.slice(0, 10);
+    setMentionHandles(sortedPersons.slice(0, 10));
   };
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const checkIfDropdownFits = () => {
+    if (dropdownRef?.current === null) return;
+    const dropdown = dropdownRef.current;
+    const x = dropdown.offsetLeft;
+    const y = dropdown.offsetTop;
+    const elementHeight = dropdown.clientHeight;
+    const elementWidht = dropdown.clientWidth;
+
+    const isVisibleOnBottom = window.innerHeight - y - elementHeight;
+    if (isVisibleOnBottom <= 0) {
+      setShowOnTop(true);
+    }
+
+    const isVisibleOnRight = window.innerWidth - x - elementWidht;
+    if (isVisibleOnRight <= 0) {
+      setShowOnRight(true);
+    }
+  };
+
+  const addMention = (event: MouseEvent<HTMLElement>, user: User) => {
+    event.preventDefault();
+    console.log(user);
+  };
+
   return (
     <div
-      className="border border-red-700 absolute mt-8 w-52 max-w-full"
+      className="border rounded absolute mt-8 w-52 max-w-full bg-white z-50"
       style={{
         left: position.x,
         top: position.y,
+        translate: showOnTop ? `0 calc(-100% + -32px)` : "0 0",
+
+        transform: showOnRight
+          ? `translateX(calc(-100% + -32px))`
+          : "translateX(0)",
       }}
+      ref={dropdownRef}
     >
       <ul>
-        {handlePersons()?.map((item) => (
-          <li key={item.email}>{item.first_name}</li>
+        {mentionHandles.map((item) => (
+          <li key={item.email} className="border-b last:border-0">
+            <button
+              className="py-1 px-2 cursor-pointer w-full text-left hover:bg-slate-300"
+              onClick={(e) => addMention(e, item)}
+            >
+              {item.first_name}
+            </button>
+          </li>
         ))}
       </ul>
     </div>
